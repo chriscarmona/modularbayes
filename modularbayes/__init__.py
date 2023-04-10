c95850caef4761e01b28efdca57d31abc8aa0a38
Chris U Carmona <carmona@stats.ox.ac.uk>
2023-04-11 01:58:28 +0200

    new version on pypi

--- modularbayes/__init__.py
+++ modularbayes/__init__.py
@@ -1,6 +1,6 @@
 """modularbayes: Variational methods for Bayesian Semi-Modular Inference."""
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from modularbayes._src.bijectors.blockwise import Blockwise
 from modularbayes._src.bijectors.conditional_bijector import ConditionalBijector
